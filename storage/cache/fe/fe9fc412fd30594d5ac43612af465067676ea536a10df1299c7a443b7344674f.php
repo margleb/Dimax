64bd70192eb44f2b12649172cb50f18b0709a2b3
margleb <margleb@yandex.ru>
2018-05-16 14:27:55 +0300

    сommit

---
+++ storage/cache/fe/fe9fc412fd30594d5ac43612af465067676ea536a10df1299c7a443b7344674f.php
@@ -0,0 +1,422 @@
+<?php
+
+/* dimaxnew/template/common/header.twig */
+class __TwigTemplate_bcc0ee001bc3f6128724112690b352e38c912d72c6b5238b154103c4227b26c4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<!--[if IE 8 ]><html dir=\"";
+        // line 3
+        echo (isset($context["direction"]) ? $context["direction"] : null);
+        echo "\" lang=\"";
+        echo (isset($context["lang"]) ? $context["lang"] : null);
+        echo "\" class=\"ie8\"><![endif]-->
+<!--[if IE 9 ]><html dir=\"";
+        // line 4
+        echo (isset($context["direction"]) ? $context["direction"] : null);
+        echo "\" lang=\"";
+        echo (isset($context["lang"]) ? $context["lang"] : null);
+        echo "\" class=\"ie9\"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html dir=\"";
+        // line 6
+        echo (isset($context["direction"]) ? $context["direction"] : null);
+        echo "\" lang=\"";
+        echo (isset($context["lang"]) ? $context["lang"] : null);
+        echo "\">
+<!--<![endif]-->
+<head>
+<meta charset=\"UTF-8\" />
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+<title>";
+        // line 12
+        echo (isset($context["title"]) ? $context["title"] : null);
+        echo "</title>
+<base href=\"";
+        // line 13
+        echo (isset($context["base"]) ? $context["base"] : null);
+        echo "\" />
+";
+        // line 14
+        if ((isset($context["description"]) ? $context["description"] : null)) {
+            // line 15
+            echo "<meta name=\"description\" content=\"";
+            echo (isset($context["description"]) ? $context["description"] : null);
+            echo "\" />
+";
+        }
+        // line 17
+        if ((isset($context["keywords"]) ? $context["keywords"] : null)) {
+            // line 18
+            echo "<meta name=\"keywords\" content=\"";
+            echo (isset($context["keywords"]) ? $context["keywords"] : null);
+            echo "\" />
+";
+        }
+        // line 20
+        echo "<script src=\"catalog/view/javascript/jquery/jquery-2.1.1.min.js\" type=\"text/javascript\"></script>
+<link href=\"catalog/view/javascript/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\" />
+<script src=\"catalog/view/javascript/bootstrap/js/bootstrap.min.js\" type=\"text/javascript\"></script>
+<link href=\"catalog/view/javascript/font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link href=\"//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700\" rel=\"stylesheet\" type=\"text/css\" />
+<link href=\"catalog/view/theme/dimaxnew/stylesheet/stylesheet.css\" rel=\"stylesheet\">
+";
+        // line 26
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["styles"]) ? $context["styles"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["style"]) {
+            // line 27
+            echo "<link href=\"";
+            echo $this->getAttribute($context["style"], "href", array());
+            echo "\" type=\"text/css\" rel=\"";
+            echo $this->getAttribute($context["style"], "rel", array());
+            echo "\" media=\"";
+            echo $this->getAttribute($context["style"], "media", array());
+            echo "\" />
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['style'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 29
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["scripts"]) ? $context["scripts"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["script"]) {
+            // line 30
+            echo "<script src=\"";
+            echo $context["script"];
+            echo "\" type=\"text/javascript\"></script>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['script'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 32
+        echo "<script src=\"catalog/view/javascript/common.js\" type=\"text/javascript\"></script>
+";
+        // line 33
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["links"]) ? $context["links"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["link"]) {
+            // line 34
+            echo "<link href=\"";
+            echo $this->getAttribute($context["link"], "href", array());
+            echo "\" rel=\"";
+            echo $this->getAttribute($context["link"], "rel", array());
+            echo "\" />
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['link'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 36
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["analytics"]) ? $context["analytics"] : null));
+        foreach ($context['_seq'] as $context["_key"] => $context["analytic"]) {
+            // line 37
+            echo $context["analytic"];
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['analytic'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "</head>
+<body>
+
+<div class=\"alert-price\">
+\t\t\t<p>Самая низкая цена на ламинат! Нашли дешевле? Сделаем скидку.</p>
+</div>
+
+<nav id=\"top\">
+
+  <div class=\"container\">
+
+\t\t<div class=\"row\">
+\t\t
+\t\t<div class=\"col-sm-6\">
+\t\t\t<ul class=\"list-inline left-menu\">
+\t\t\t\t<li><a href=\"#\">Доставка и самовывоз</a></li>
+\t\t\t\t<li><a href=\"#\">Cтатьи и идеи</a></li>
+\t\t\t\t<li><a href=\"#\">FAQ</a></li>
+\t\t\t\t<li><a href=\"#\">Оплата</a></li>
+\t\t\t</ul>
+\t\t</div>
+\t\t<div class=\"col-sm-6\">
+\t\t    <div id=\"top-links\" class=\"nav\">
+      <ul class=\"list-inline right-menu\">
+        ";
+        // line 64
+        echo "        <li class=\"dropdown\"><a href=\"";
+        echo (isset($context["account"]) ? $context["account"] : null);
+        echo "\" title=\"";
+        echo (isset($context["text_account"]) ? $context["text_account"] : null);
+        echo "\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"fa fa-user\"></i> <span class=\"hidden-xs hidden-sm\">";
+        echo (isset($context["text_account"]) ? $context["text_account"] : null);
+        echo "</span> <span class=\"caret\"></span></a>
+          <ul class=\"dropdown-menu dropdown-menu-right\">
+            ";
+        // line 66
+        if ((isset($context["logged"]) ? $context["logged"] : null)) {
+            // line 67
+            echo "            <li><a href=\"";
+            echo (isset($context["account"]) ? $context["account"] : null);
+            echo "\">";
+            echo (isset($context["text_account"]) ? $context["text_account"] : null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 68
+            echo (isset($context["order"]) ? $context["order"] : null);
+            echo "\">";
+            echo (isset($context["text_order"]) ? $context["text_order"] : null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 69
+            echo (isset($context["transaction"]) ? $context["transaction"] : null);
+            echo "\">";
+            echo (isset($context["text_transaction"]) ? $context["text_transaction"] : null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 70
+            echo (isset($context["download"]) ? $context["download"] : null);
+            echo "\">";
+            echo (isset($context["text_download"]) ? $context["text_download"] : null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 71
+            echo (isset($context["logout"]) ? $context["logout"] : null);
+            echo "\">";
+            echo (isset($context["text_logout"]) ? $context["text_logout"] : null);
+            echo "</a></li>
+            ";
+        } else {
+            // line 73
+            echo "            <li><a href=\"";
+            echo (isset($context["register"]) ? $context["register"] : null);
+            echo "\">";
+            echo (isset($context["text_register"]) ? $context["text_register"] : null);
+            echo "</a></li>
+            <li><a href=\"";
+            // line 74
+            echo (isset($context["login"]) ? $context["login"] : null);
+            echo "\">";
+            echo (isset($context["text_login"]) ? $context["text_login"] : null);
+            echo "</a></li>
+            ";
+        }
+        // line 76
+        echo "          </ul>
+        </li>
+        <li><a href=\"";
+        // line 78
+        echo (isset($context["wishlist"]) ? $context["wishlist"] : null);
+        echo "\" id=\"wishlist-total\" title=\"";
+        echo (isset($context["text_wishlist"]) ? $context["text_wishlist"] : null);
+        echo "\"><i class=\"fa fa-heart\"></i> <span class=\"hidden-xs hidden-sm\">";
+        echo (isset($context["text_wishlist"]) ? $context["text_wishlist"] : null);
+        echo "</span></a></li>
+        <li><a href=\"";
+        // line 79
+        echo (isset($context["shopping_cart"]) ? $context["shopping_cart"] : null);
+        echo "\" title=\"";
+        echo (isset($context["text_shopping_cart"]) ? $context["text_shopping_cart"] : null);
+        echo "\"><i class=\"fa fa-shopping-cart\"></i> <span class=\"hidden-xs hidden-sm\">";
+        echo (isset($context["text_shopping_cart"]) ? $context["text_shopping_cart"] : null);
+        echo "</span></a></li>
+      </ul>
+    </div>
+\t\t
+\t\t</div>
+\t\t
+\t\t</div>
+
+
+  </div>
+</nav>
+<header>
+  <div class=\"container\">
+    <div class=\"row\">
+      <div class=\"col-sm-3\">
+        <div id=\"logo\">";
+        // line 94
+        if ((isset($context["logo"]) ? $context["logo"] : null)) {
+            echo "<a href=\"";
+            echo (isset($context["home"]) ? $context["home"] : null);
+            echo "\"><img src=\"";
+            echo (isset($context["logo"]) ? $context["logo"] : null);
+            echo "\" title=\"";
+            echo (isset($context["name"]) ? $context["name"] : null);
+            echo "\" alt=\"";
+            echo (isset($context["name"]) ? $context["name"] : null);
+            echo "\" class=\"img-responsive\" /></a>";
+        } else {
+            // line 95
+            echo "          <h1><a href=\"";
+            echo (isset($context["home"]) ? $context["home"] : null);
+            echo "\">";
+            echo (isset($context["name"]) ? $context["name"] : null);
+            echo "</a></h1>
+          ";
+        }
+        // line 96
+        echo "</div>
+      </div>
+      <div class=\"col-sm-5\">";
+        // line 98
+        echo (isset($context["search"]) ? $context["search"] : null);
+        echo "</div>
+      <div class=\"col-sm-4\">
+\t\t\t<ul class=\"сontacts\">
+\t\t\t<li><i class=\"fa fa-map-marker\" aria-hidden=\"true\"></i>Санкт-Петербург, ул. Варфоломеевская, 6</li>
+\t\t\t<li><i class=\"fa fa-phone\" aria-hidden=\"true\"></i>+7 (812) 627-61-25, +7 (931) 276-53-60</li>
+\t\t\t</ul>
+\t\t\t</div>
+    </div>
+  </div>
+</header>
+";
+        // line 108
+        echo (isset($context["menu"]) ? $context["menu"] : null);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "dimaxnew/template/common/header.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  294 => 108,  281 => 98,  277 => 96,  269 => 95,  257 => 94,  235 => 79,  227 => 78,  223 => 76,  216 => 74,  209 => 73,  202 => 71,  196 => 70,  190 => 69,  184 => 68,  177 => 67,  175 => 66,  165 => 64,  139 => 39,  131 => 37,  127 => 36,  116 => 34,  112 => 33,  109 => 32,  100 => 30,  96 => 29,  83 => 27,  79 => 26,  71 => 20,  65 => 18,  63 => 17,  57 => 15,  55 => 14,  51 => 13,  47 => 12,  36 => 6,  29 => 4,  23 => 3,  19 => 1,);
+    }
+}
+/* <!DOCTYPE html>*/
+/* <!--[if IE]><![endif]-->*/
+/* <!--[if IE 8 ]><html dir="{{ direction }}" lang="{{ lang }}" class="ie8"><![endif]-->*/
+/* <!--[if IE 9 ]><html dir="{{ direction }}" lang="{{ lang }}" class="ie9"><![endif]-->*/
+/* <!--[if (gt IE 9)|!(IE)]><!-->*/
+/* <html dir="{{ direction }}" lang="{{ lang }}">*/
+/* <!--<![endif]-->*/
+/* <head>*/
+/* <meta charset="UTF-8" />*/
+/* <meta name="viewport" content="width=device-width, initial-scale=1">*/
+/* <meta http-equiv="X-UA-Compatible" content="IE=edge">*/
+/* <title>{{ title }}</title>*/
+/* <base href="{{ base }}" />*/
+/* {% if description %}*/
+/* <meta name="description" content="{{ description }}" />*/
+/* {% endif %}*/
+/* {% if keywords %}*/
+/* <meta name="keywords" content="{{ keywords }}" />*/
+/* {% endif %}*/
+/* <script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>*/
+/* <link href="catalog/view/javascript/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />*/
+/* <script src="catalog/view/javascript/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>*/
+/* <link href="catalog/view/javascript/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />*/
+/* <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />*/
+/* <link href="catalog/view/theme/dimaxnew/stylesheet/stylesheet.css" rel="stylesheet">*/
+/* {% for style in styles %}*/
+/* <link href="{{ style.href }}" type="text/css" rel="{{ style.rel }}" media="{{ style.media }}" />*/
+/* {% endfor %}*/
+/* {% for script in scripts %}*/
+/* <script src="{{ script }}" type="text/javascript"></script>*/
+/* {% endfor %}*/
+/* <script src="catalog/view/javascript/common.js" type="text/javascript"></script>*/
+/* {% for link in links %}*/
+/* <link href="{{ link.href }}" rel="{{ link.rel }}" />*/
+/* {% endfor %}*/
+/* {% for analytic in analytics %}*/
+/* {{ analytic }}*/
+/* {% endfor %}*/
+/* </head>*/
+/* <body>*/
+/* */
+/* <div class="alert-price">*/
+/* 			<p>Самая низкая цена на ламинат! Нашли дешевле? Сделаем скидку.</p>*/
+/* </div>*/
+/* */
+/* <nav id="top">*/
+/* */
+/*   <div class="container">*/
+/* */
+/* 		<div class="row">*/
+/* 		*/
+/* 		<div class="col-sm-6">*/
+/* 			<ul class="list-inline left-menu">*/
+/* 				<li><a href="#">Доставка и самовывоз</a></li>*/
+/* 				<li><a href="#">Cтатьи и идеи</a></li>*/
+/* 				<li><a href="#">FAQ</a></li>*/
+/* 				<li><a href="#">Оплата</a></li>*/
+/* 			</ul>*/
+/* 		</div>*/
+/* 		<div class="col-sm-6">*/
+/* 		    <div id="top-links" class="nav">*/
+/*       <ul class="list-inline right-menu">*/
+/*         {# <li><i class="fa fa-phone"></i><span class="hidden-xs hidden-sm">{{ telephone }}</span></li> #}*/
+/*         <li class="dropdown"><a href="{{ account }}" title="{{ text_account }}" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <span class="hidden-xs hidden-sm">{{ text_account }}</span> <span class="caret"></span></a>*/
+/*           <ul class="dropdown-menu dropdown-menu-right">*/
+/*             {% if logged %}*/
+/*             <li><a href="{{ account }}">{{ text_account }}</a></li>*/
+/*             <li><a href="{{ order }}">{{ text_order }}</a></li>*/
+/*             <li><a href="{{ transaction }}">{{ text_transaction }}</a></li>*/
+/*             <li><a href="{{ download }}">{{ text_download }}</a></li>*/
+/*             <li><a href="{{ logout }}">{{ text_logout }}</a></li>*/
+/*             {% else %}*/
+/*             <li><a href="{{ register }}">{{ text_register }}</a></li>*/
+/*             <li><a href="{{ login }}">{{ text_login }}</a></li>*/
+/*             {% endif %}*/
+/*           </ul>*/
+/*         </li>*/
+/*         <li><a href="{{ wishlist }}" id="wishlist-total" title="{{ text_wishlist }}"><i class="fa fa-heart"></i> <span class="hidden-xs hidden-sm">{{ text_wishlist }}</span></a></li>*/
+/*         <li><a href="{{ shopping_cart }}" title="{{ text_shopping_cart }}"><i class="fa fa-shopping-cart"></i> <span class="hidden-xs hidden-sm">{{ text_shopping_cart }}</span></a></li>*/
+/*       </ul>*/
+/*     </div>*/
+/* 		*/
+/* 		</div>*/
+/* 		*/
+/* 		</div>*/
+/* */
+/* */
+/*   </div>*/
+/* </nav>*/
+/* <header>*/
+/*   <div class="container">*/
+/*     <div class="row">*/
+/*       <div class="col-sm-3">*/
+/*         <div id="logo">{% if logo %}<a href="{{ home }}"><img src="{{ logo }}" title="{{ name }}" alt="{{ name }}" class="img-responsive" /></a>{% else %}*/
+/*           <h1><a href="{{ home }}">{{ name }}</a></h1>*/
+/*           {% endif %}</div>*/
+/*       </div>*/
+/*       <div class="col-sm-5">{{ search }}</div>*/
+/*       <div class="col-sm-4">*/
+/* 			<ul class="сontacts">*/
+/* 			<li><i class="fa fa-map-marker" aria-hidden="true"></i>Санкт-Петербург, ул. Варфоломеевская, 6</li>*/
+/* 			<li><i class="fa fa-phone" aria-hidden="true"></i>+7 (812) 627-61-25, +7 (931) 276-53-60</li>*/
+/* 			</ul>*/
+/* 			</div>*/
+/*     </div>*/
+/*   </div>*/
+/* </header>*/
+/* {{ menu }}*/
+/* */
